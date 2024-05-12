e93f6363547c8b48e772e3b9409d0030b320070f
codebreaker496 <mamun.abdulla@gmail.com>
2024-05-12 07:21:34 +0600

    Menu function added

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+var menu = document.querySelector("#nav i");
+var close = document.querySelector(".overlay i");
+
+
 var tl = gsap.timeline();
 tl.to(".overlay", {
     right:0,
+    ease: "back.in(1.2)",
     duration:1,
 })
 tl.from(".overlay h4", {
@@ -8,4 +13,23 @@ tl.from(".overlay h4", {
     duration:.5,
     stagger:.3,
     opacity:0,
+})
+
+tl.pause();
+// Functionality for open menu & close menu.
+
+menu.addEventListener("click", () => {
+    tl.play();
+})
+
+close.addEventListener("click", () => {
+    tl.reverse();
+})
+
+// hide menu on clicking outside of it.
+
+document.addEventListener("click", (e) => {
+    if(e.target !== menu && e.target !== close){
+        tl.reverse();
+    }
 })
